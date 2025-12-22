ef92bf443cb09b9651b977723e4a0514d6dfd172
ayushdutta7318 <ayushdutta7318@gmail.com>
2025-12-22 11:03:52 +0530

    Add files via upload

---
+++ 5_CmdLineArguments/1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#command line args:
+
+echo "The first argument is: $1";
+echo "The second argument is: $2";
+
+echo "All the areguments are: $@";
+echo "Number of arguments entered are: $#";
+
+
---
+++ 5_CmdLineArguments/2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#assignment on cmd line args:
+
+echo "hello $1, your favorite color is $2";
+
+
