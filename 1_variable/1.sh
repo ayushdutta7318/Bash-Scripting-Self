67e1a81e919d189f7adfc877088ebdbbe51be39f
ayushdutta7318 <ayushdutta7318@gmail.com>
2025-12-22 11:15:09 +0530

    Add files via upload

---
+++ 1_variable/1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Hello, World!";
+
+echo "This is my first bash script!";
+
+#variables:
+
+NAME="Ayush";
+
+echo "Welcome,$NAME";
+
+#take user input
+#
+echo "Enter your name";
+read NAME_1;
+
+echo "Good Morning, $NAME_1";
---
+++ 1_variable/2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Enter you name:"
+read NAME;
+
+echo "Enter your job:"
+read JOB
+
+echo "NAME: $NAME, JOB: $JOB";
