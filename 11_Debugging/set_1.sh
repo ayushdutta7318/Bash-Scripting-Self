81aff038d49a37a34fa1ab4703b94aeaa3767253
ayushdutta7318 <ayushdutta7318@gmail.com>
2025-12-28 14:54:17 +0530

    Add files via upload

---
+++ 11_Debugging/set_1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#debugging:
+
+set -x;
+set -u;
+
+#debug on: applied where debug necessary. b/w debug on and off.
+NAME="ayush";
+echo "hello, $NAME";
+#debug off;
+
+VAR=
+
+echo "I am in debugging script";
+exit 0;
---
+++ 11_Debugging/set_2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#trio of debugging in bash
+
+set -euo pipefail;
+
+
+hjbgfdjv
+VAR=
+
+echo "I am $VAR";
+
+exit 0;
